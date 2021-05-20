f437c2cd373c524c007f885f5a1ba2cc42156070
unknown <kitolghj@gmail.com>
2021-05-21 05:20:46 +0800

    Added eslint and editorconfig

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
-import configureStore from 'src/redux/store'
+import configureStore from './src/redux/store'
 
 const store = configureStore()
 
@@ -10,9 +10,16 @@ export default function App() {
     <Provider store={store}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
       </View>
     </Provider>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
